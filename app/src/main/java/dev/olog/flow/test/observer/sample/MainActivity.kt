9ae0d258de355552f07fbd514915907f1e04b2a3
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-01-25 19:59:16 +0100

    removed unused resources

--- app/src/main/java/dev/olog/flow/test/observer/sample/MainActivity.kt
+++ app/src/main/java/dev/olog/flow/test/observer/sample/MainActivity.kt
@@ -1,13 +1,5 @@
 package dev.olog.flow.test.observer.sample
 
 import android.app.Activity
-import android.os.Bundle
 
-class MainActivity : Activity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-    }
-}
+class MainActivity : Activity()
