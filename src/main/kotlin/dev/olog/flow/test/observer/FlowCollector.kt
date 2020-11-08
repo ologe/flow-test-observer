b55bf67e1e123f610b44fefa34fe142d97d0056c
Eugeniu Olog <eugeniu.olog@ynap.com>
2020-11-08 16:56:37 +0100

    deleted deprecated Flow<T>#test()

--- src/main/kotlin/dev/olog/flow/test/observer/FlowCollector.kt
+++ src/main/kotlin/dev/olog/flow/test/observer/FlowCollector.kt
@@ -6,35 +6,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import kotlin.coroutines.coroutineContext
 
-/**
- * Allows hot and cold [Flow] streams testing.
- * [Hot vs Cold steams](https://medium.com/@luukgruijs/understanding-hot-vs-cold-observables-62d04cf92e03)
- *
- * Usage example (using a cold flow here for simplicity):
- * ```
- * val flow = flow {
- *      emit(1)
- *      emit(2)
- *  }
- *
- * flow.test()
- *      .assertValues(1, 2, 3)
- *      .assertValueCount(3)
- *
- */
-@Suppress("DeprecatedCallableAddReplaceWith")
-@Deprecated(
-    message = """
-        Please use the new version [test(CoroutineScope, FlowTestCollector.() -> Unit)]. This version has problems with 
-        broadcast channels because it consumes all the items synchronously. The new version launches the [FlowTestCollector]
-        inside a new coroutine to solve the problem.
-        (Sorry but I cannot provide a replaceWith helper because of [this] keyword)
-    """
-)
-fun <T> Flow<T>.test(): FlowTestCollector<T> {
-    return FlowTestCollectorImpl(this)
-}
-
 // since 1.4.0
 /**
  * Allows hot and cold [Flow] streams testing.
